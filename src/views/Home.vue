ef3dd72895d1b6446ffcb0f3872ff1964e41fe51
frank091915 <frank091915@163.com>
2020-05-18 17:04:59 +0800

    lint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,10 +20,8 @@
         <div class="blockOne block">
           <div class="left">
             <div class="icon iconOne">
-              <div class="whiteStripe">
-
-              </div>
-              <p class="iconDesciption"> 7Days</p>
+              <div class="whiteStripe"></div>
+              <p class="iconDesciption">7Days</p>
             </div>
             <div class="description">
               <div class="title">
@@ -48,12 +46,9 @@
                   <span class="singleFeatureOne">速热10秒淋浴</span>
                 </div>
               </div>
-
             </div>
           </div>
-          <div class="right">
-
-          </div>
+          <div class="right"></div>
         </div>
       </div>
     </div>
@@ -103,148 +98,147 @@ export default {
 }
 
 .rcmd-center-wrapper {
-    width: 12rem;
-    height: 5.3rem;
-    border-radius: 0.47rem;
-    position: absolute;
-    top: 2.19rem;
-    left: 50%;
-    margin-left: -6.1rem;
-    z-index: 998;
-    // background: url('../../public/images/recommendTip.png') no-repeat;
-    background-size: contain;
-    text-align: center;
-    background-color: #2156af;
+  width: 12rem;
+  height: 5.3rem;
+  border-radius: 0.47rem;
+  position: absolute;
+  top: 2.19rem;
+  left: 50%;
+  margin-left: -6.1rem;
+  z-index: 998;
+  // background: url('../../public/images/recommendTip.png') no-repeat;
+  background-size: contain;
+  text-align: center;
+  background-color: #2156af;
 
-    .rcmd-center-wrapper-image {
-      width: 10.3rem;
-      height: 3.93rem;
-      background: rgba(255, 255, 255, 1);
-      // background-image: url('../../public/images/rcmd-center-bcImage.jpg');
-      border: 1px solid rgba(236, 236, 236, 1);
-      border-radius: 10px;
-      display: block;
-      margin: 0.42rem auto;
-      cursor: pointer;
-    }
-    .rcmd-center-name {
-      font-size: 0.25rem;
-      color: #fff;
-    }
+  .rcmd-center-wrapper-image {
+    width: 10.3rem;
+    height: 3.93rem;
+    background: rgba(255, 255, 255, 1);
+    // background-image: url('../../public/images/rcmd-center-bcImage.jpg');
+    border: 1px solid rgba(236, 236, 236, 1);
+    border-radius: 10px;
+    display: block;
+    margin: 0.42rem auto;
+    cursor: pointer;
   }
-
-  // 7天通用标志样式
-  .icon{
-    width: 1.15rem;
-    position: absolute;
-    text-align: center;
-  }
-
-  .iconDesciption{
-    font-size: .29rem;
-    line-height: .54rem;
-    font-family: SourceHanSansCN;
-    font-weight: 400;
+  .rcmd-center-name {
+    font-size: 0.25rem;
     color: #fff;
-    margin-top: 0.1rem;
+  }
+}
+
+// 7天通用标志样式
+.icon {
+  width: 1.15rem;
+  position: absolute;
+  text-align: center;
+}
+
+.iconDesciption {
+  font-size: 0.29rem;
+  line-height: 0.54rem;
+  font-family: SourceHanSansCN;
+  font-weight: 400;
+  color: #fff;
+  margin-top: 0.1rem;
+}
+
+.whiteStripe {
+  width: 100%;
+  height: 0.29rem;
+  background: #fff;
+  margin-bottom: 0.1rem;
+}
+
+.singleFeature {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: 0.48rem;
+}
+
+.singleFeatureIcon {
+  width: 0.4rem;
+  height: 0.4rem;
+  margin-right: 0.09rem;
+}
+
+.picAndInstructionBlock {
+  .block {
+    position: relative;
   }
 
-  .whiteStripe{
-    width: 100%;
-    height: .29rem;
-    background: #fff;
-    margin-bottom: 0.1rem;
-  }
+  .blockOne {
+    height: 9.46rem;
+    border: 1px solid lightblue;
 
-  .singleFeature{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-start;
-    margin-bottom: 0.48rem;
-  }
+    .left {
+      width: 50%;
+      height: 100%;
+      float: left;
+      box-sizing: border-box;
+      background-color: #eaa39e;
 
-  .singleFeatureIcon{
-    width: 0.4rem;
-    height: 0.4rem;
-    margin-right: 0.09rem;
-  }
+      .iconOne {
+        top: 0.86rem;
+        left: 0.73rem;
+      }
 
-  .picAndInstructionBlock{
+      .description {
+        position: absolute;
+        top: 1.68rem;
+        left: 4.16rem;
 
-    .block{
-      position: relative;
-    }
+        .title {
+          height: 1.18rem;
+          font-size: 0.42rem;
+          line-height: 0.42rem;
+          color: #fff;
+          font-family: SourceHanSansCN;
+          border-left: 0.21rem solid #fff;
+          margin-bottom: 0.88rem;
+          text-indent: 0.29rem;
 
-    .blockOne{
-      height:9.46rem;
-      border: 1px solid lightblue;
-
-      .left{
-        width: 50%;
-        height: 100%;
-        float: left;
-        box-sizing: border-box;
-        background-color: #EAA39E;
-
-        .iconOne{
-          top: .86rem;
-          left: .73rem;
+          .singleTitle {
+            text-align: left;
+            margin-bottom: 0.38rem;
+          }
         }
 
-        .description{
-          position: absolute;
-          top: 1.68rem;
-          left: 4.16rem;
+        .features {
+          font-size: 0.29rem;
+          color: #fff;
+          font-family: SourceHanSansCN;
 
-          .title{
-            height: 1.18rem;
-            font-size: 0.42rem;
-            line-height: 0.42rem;
-            color: #fff;
-            font-family: SourceHanSansCN;
-            border-left: .21rem solid #fff;
-            margin-bottom: 0.88rem;
-            text-indent: 0.29rem;
-
-            .singleTitle{
-              text-align: left;
-              margin-bottom: 0.38rem;
-            }
+          .icon-one {
+            background: url("../../public/images/block-one-icon-star.png");
+            background-size: 100%;
           }
-
-          .features{
-            font-size: 0.29rem;
-            color: #fff;
-            font-family: SourceHanSansCN;
-
-            .icon-one{
-              background: url("../../public/images/block-one-icon-star.png");
-              background-size: 100%;
-            }
-            .icon-two{
-              background: url("../../public/images/block-one-icon-washing.png");
-              background-size: 100%;
-            }
-            .icon-three{
-              background: url("../../public/images/block-one-icon-wifi.png");
-              background-size: 100%;
-            }
-            .icon-four{
-              background: url("../../public/images/block-one-icon-shower.png");
-              background-size: 100%;
-            }
+          .icon-two {
+            background: url("../../public/images/block-one-icon-washing.png");
+            background-size: 100%;
+          }
+          .icon-three {
+            background: url("../../public/images/block-one-icon-wifi.png");
+            background-size: 100%;
+          }
+          .icon-four {
+            background: url("../../public/images/block-one-icon-shower.png");
+            background-size: 100%;
           }
         }
       }
+    }
 
-      .right{
-        width: 50%;
-        height: 100%;
-        float: right;
-        background: url("../../public/images/block-one-rightBc.jpg");
-        background-size: 100%;
-      }
+    .right {
+      width: 50%;
+      height: 100%;
+      float: right;
+      background: url("../../public/images/block-one-rightBc.jpg");
+      background-size: 100%;
     }
   }
+}
 </style>
