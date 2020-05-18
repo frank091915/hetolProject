38bf64b0ecdc1aae9875a671d667ba6b5eb41026
frank091915 <frank091915@163.com>
2020-05-18 11:44:02 +0800

    update

--- src/components/investmentService.vue
+++ src/components/investmentService.vue
@@ -1,7 +1,7 @@
 <template>
-<div class=''>
+  <div class="">
     <div class="fw-img"></div>
-</div>
+  </div>
 </template>
 
 <script>
@@ -9,48 +9,40 @@
 //例如：import 《组件名称》 from '《组件路径》';
 
 export default {
-//import引入的组件需要注入到对象中才能使用
-components: {},
-data() {
-//这里存放数据
-return {
-
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    //这里存放数据
+    return {};
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
 };
-},
-//监听属性 类似于data概念
-computed: {},
-//监控data中的数据变化
-watch: {},
-//方法集合
-methods: {
-
-},
-//生命周期 - 创建完成（可以访问当前this实例）
-created() {
-
-},
-//生命周期 - 挂载完成（可以访问DOM元素）
-mounted() {
-
-},
-beforeCreate() {}, //生命周期 - 创建之前
-beforeMount() {}, //生命周期 - 挂载之前
-beforeUpdate() {}, //生命周期 - 更新之前
-updated() {}, //生命周期 - 更新之后
-beforeDestroy() {}, //生命周期 - 销毁之前
-destroyed() {}, //生命周期 - 销毁完成
-activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
-}
 </script>
 <style scoped>
-    .fw-img {
-        position: fixed;
-        right: 20px;
-        top: 50%;
-        width: 2.16rem;
-        height: 2.18rem;
-        z-index: 999;
-        background-image: url('../../public/images/fw.png');
-        background-size: cover;
-    }
+.fw-img {
+  position: fixed;
+  right: 20px;
+  top: 50%;
+  width: 2.16rem;
+  height: 2.18rem;
+  z-index: 999;
+  background-image: url("../../public/images/fw.png");
+  background-size: cover;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,13 @@
     </div>
     <!-- 推荐 -->
     <div class="recommend">
-      <div class="recommend-header-bg">
-      </div>
-      <div class="recommendTip">
-      </div>
+      <div class="recommend-header-bg"></div>
+      <div class="recommendTip"></div>
       <div class="rcmd-center-wrapper">
-        <img src='../../public/images/rcmd-center-bcImage.jpg' class="rcmd-center-wrapper-image">
+        <img
+          src="../../public/images/rcmd-center-bcImage.jpg"
+          class="rcmd-center-wrapper-image"
+        />
         <p class="rcmd-center-name">7天优品Premium · 深圳科技园店</p>
       </div>
     </div>
@@ -22,7 +23,7 @@
 <script>
 // @ is an alias to /src
 import Swiper from "@/components/swiper";
-import InvestmentService from "@/components/investmentService"
+import InvestmentService from "@/components/investmentService";
 export default {
   name: "Home",
   components: {
@@ -36,60 +37,59 @@ export default {
 .home {
   font-size: 0.2rem;
 }
-.recommend{
+.recommend {
   position: relative;
 }
-.recommend-header-bg{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 6.09rem;
-    background-image: url('../../public/images/recommend-header-bg.png');
-    background-size: cover;
+.recommend-header-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 6.09rem;
+  background-image: url("../../public/images/recommend-header-bg.png");
+  background-size: cover;
 }
-.recommendTip{
+.recommendTip {
   width: 1.93rem;
   height: 1.09rem;
   display: inline-block;
   z-index: 2;
   position: absolute;
-  top: .58rem;
+  top: 0.58rem;
   left: 50%;
   margin-left: -0.965rem;
-  background-image: url('../../public/images/recommendTip.png');
+  background-image: url("../../public/images/recommendTip.png");
   background-size: cover;
 }
 
-.rcmd-center-wrapper{
-    width: 12rem;
-    height: 5.3rem;
-    border-radius: .47rem;
-    position: absolute;
-    top: 2.19rem;
-    left: 50%;
-    margin-left: -6.1rem;
-    z-index: 998;
-    // background: url('../../public/images/recommendTip.png') no-repeat;
-    background-size: contain;
-    text-align: center;
-    background-color: #2156AF;
+.rcmd-center-wrapper {
+  width: 12rem;
+  height: 5.3rem;
+  border-radius: 0.47rem;
+  position: absolute;
+  top: 2.19rem;
+  left: 50%;
+  margin-left: -6.1rem;
+  z-index: 998;
+  // background: url('../../public/images/recommendTip.png') no-repeat;
+  background-size: contain;
+  text-align: center;
+  background-color: #2156af;
 
-    .rcmd-center-wrapper-image{
-        width: 10.3rem;
-        height: 3.93rem;
-        background: rgba(255, 255, 255, 1) ;
-        // background-image: url('../../public/images/rcmd-center-bcImage.jpg');
-        border: 1px solid rgba(236, 236, 236, 1);
-        border-radius: 10px;
-        display: block;
-        margin: .42rem auto;
-        cursor: pointer;
-    }
-    .rcmd-center-name{
-      font-size: 0.25rem;
-      color: #fff;
-    }
+  .rcmd-center-wrapper-image {
+    width: 10.3rem;
+    height: 3.93rem;
+    background: rgba(255, 255, 255, 1);
+    // background-image: url('../../public/images/rcmd-center-bcImage.jpg');
+    border: 1px solid rgba(236, 236, 236, 1);
+    border-radius: 10px;
+    display: block;
+    margin: 0.42rem auto;
+    cursor: pointer;
+  }
+  .rcmd-center-name {
+    font-size: 0.25rem;
+    color: #fff;
+  }
 }
-
 </style>
